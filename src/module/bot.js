93dee7d9313b2877d7e3a141fc0324767fdce5be
U.V.Phani Charan <phanicharan116@gmail.com>
2025-02-23 15:42:52 +0530

    details update

--- src/module/bot.js
+++ src/module/bot.js
@@ -4,10 +4,11 @@ function TawkToWidget() {
   useEffect(() => {
     // ComponentDidMount equivalent
     // var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    const tawkToSrc = process.env.REACT_APP_TAWKTO_SRC;
     (function(){
       var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
       s1.async=true;
-      s1.src='https://embed.tawk.to/65eee9898d261e1b5f6b7d2e/1homk0h1c';
+      s1.src=tawkToSrc;
       s1.charset='UTF-8';
       s1.setAttribute('crossorigin','*');
       s0.parentNode.insertBefore(s1,s0);
